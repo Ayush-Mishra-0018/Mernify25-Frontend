35c98064e40b6883092f0e711f32883ad20913b9
kartikeya-dimri <kartikeya.dimri@iiitb.ac.in>
2025-11-07 11:32:58 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4 rounded-lg text-center">
+        Hello Tailwind!
+      </div>
+    </>
+  )
+}
+
+export default App
