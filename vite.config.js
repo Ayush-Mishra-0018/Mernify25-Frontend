d97880829af01701457e87d1479f1d721d134fbf
iHarshSinha <harsh.sinha@iiitb.ac.in>
2025-11-08 10:20:07 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: './', // ensures relative asset paths work after deployment
+  build: {
+    outDir: 'dist', // this is the folder Railway should serve
+  },
 })
